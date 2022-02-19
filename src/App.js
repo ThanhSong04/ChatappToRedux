cc51e4472f2e6d9b5ae8d4b938e92885956b0e40
Thanh Song <lesong0162@gmail.com>
2022-02-19 20:05:46 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -6,15 +6,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>songdeptrai</p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
+                <p>Thanh song</p>
             </header>
         </div>
     );
